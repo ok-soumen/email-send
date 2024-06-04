0304e9debbd28aa2e2e147eaffc4e578930dbeea
Rajib Ganguly <rajibganguly.study@yahoo.in>
2024-06-04 15:24:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+
 const express = require("express");
 const app = express();
 const cors = require('cors');
 app.use(express.json({ limit: '20mb' })); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(cors()); // Enable CORS
+let PORT = 5000;
 
 const sendMail = require("./controllers/sendMail");
 
@@ -15,4 +17,12 @@ app.post("/send-mail", sendMail);
 
 
 
+const start = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`I am live in port no.  ${PORT}`);
+    });
+  } catch (error) {}
+};
 
+start()
