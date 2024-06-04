cb7bfb9c9f5c7e51451cf6392b5a8ea84880d8ce
Soumen <105904286+ok-soumen@users.noreply.github.com>
2024-06-04 13:57:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-let PORT = 5000;
 app.use(express.json({ limit: '20mb' })); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(cors()); // Enable CORS
@@ -14,12 +13,6 @@ app.get("/", (req, res) => {
 
 app.post("/send-mail", sendMail);
 
-const start = async () => {
-  try {
-    app.listen(PORT, () => {
-      console.log(`I am live in port no.  ${PORT}`);
-    });
-  } catch (error) {}
-};
+
 
 start();
